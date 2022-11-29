ab7a2e2ec93258ff7998ff0bcfdcd7722f482968
DeadSpace14 <119079484+DeadSpace14@users.noreply.github.com>
2022-11-29 08:33:48 +0300

    Изменение ссылок в лобби
    Добавлены ссылки на:
    Наш репозиторий
    Дискорд
    Багорепорт на нашем репозитории

--- Content.Client/Links/UILinks.cs
+++ Content.Client/Links/UILinks.cs
@@ -2,11 +2,11 @@
 {
     public static class UILinks
     {
-        public const string GitHub = "https://github.com/space-syndicate/space-station-14/";
+        public const string GitHub = "https://github.com/DeadSpace14/deadspace-Red/";
         public const string Patreon = "https://boosty.to/corvax";
 
-        public const string Discord = "https://discord.com/invite/F9bNgSYfX5";
-        public const string BugReport = "https://github.com/space-syndicate/space-station-14/issues/new/choose";
+        public const string Discord = "https://dsc.gg/deadspace14";
+        public const string BugReport = "https://github.com/DeadSpace14/deadspace-Red/issues/new/choose";
         public const string Website = "https://station14.ru";
         public const string Wiki = "https://wiki.station14.ru";
     }
